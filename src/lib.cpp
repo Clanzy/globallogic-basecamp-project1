02e313a4ab77adc40da1c9aa353730ed11841f92
clanzy <clanzy8@gmail.com>
2022-06-18 20:41:09 +0300

    Single-threaded search implemented

--- src/lib.cpp
+++ src/lib.cpp
@@ -3,15 +3,14 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <thread>
+#include <atomic>
 
 bool walk(const std::string &name, std::string &path, DIR *walker)
 {
     struct dirent *dir;
-    if (path == "/run/user/")
-        return false;
     while ((dir = readdir(walker)))
     {
-
         if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
         {
             continue;
@@ -19,6 +18,11 @@ bool walk(const std::string &name, std::string &path, DIR *walker)
         if (strcmp(dir->d_name, name.c_str()) == 0)
         {
             path.append(dir->d_name);
+            if (dir->d_type == DT_DIR)
+            {
+                path.append("/");
+            }
+            std::cout << path << '\n';
             return true;
         }
         if (dir->d_type == DT_DIR)
@@ -27,33 +31,28 @@ bool walk(const std::string &name, std::string &path, DIR *walker)
             DIR *another_walker = opendir(path.c_str());
             if (another_walker)
             {
-                bool isSuccessful = walk(name, path, another_walker);
+                bool hasFound = walk(name, path, another_walker);
                 closedir(another_walker);
-
-                if (isSuccessful)
-                {
+                if(hasFound){
                     return true;
                 }
             }
             path.resize(path.size() - strlen(dir->d_name) - 1);
         }
     }
-
     return false;
 }
 
-void find_path(const std::string &name, std::string &path)
+void find_path(const std::string &name)
 {
-    path.append("/");
-    DIR *walker = opendir("/");
+    std::string path = "/";
+    DIR *walker = opendir(path.c_str());
     walk(name, path, walker);
 }
 
 int main(int argc, char *argv[])
 {
-    std::string path;
     const std::string name = argv[1];
-    find_path(name, path);
-    std::cout << path << '\n';
+    find_path(name);
     return 0;
 }
